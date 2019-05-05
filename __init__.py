8c7330558b574e16cdceb77e0dd6116d6e205c65
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-05-05 15:55:05 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
+import RPi.GPIO as GPIO
+import time
 
 
 class RosGpioPins(MycroftSkill):
@@ -8,6 +10,17 @@ class RosGpioPins(MycroftSkill):
     @intent_file_handler('pins.gpio.ros.intent')
     def handle_pins_gpio_ros(self, message):
         self.speak_dialog('pins.gpio.ros')
+        #GPIO.setmode(GPIO.BOARD)
+        green=11
+        GPIO.setup(green,GPIO.OUT)
+        GPIO.output(green,True)
+        time.sleep(1)
+        GPIO.output(green,False)
+        time.sleep(1)
+        GPIO.output(green,True)
+        time.sleep(1)
+        GPIO.output(green,False)
+        GPIO.cleanup()
 
 
 def create_skill():
